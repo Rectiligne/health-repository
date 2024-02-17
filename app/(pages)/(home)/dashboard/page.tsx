cdebcf41f2812247bb229530e56e689f848b94a2
laurent <laurent-quenel@hotmail.fr>
2024-02-17 15:40:23 +0100

    :bug: fix build

--- app/(pages)/(home)/dashboard/page.tsx
+++ app/(pages)/(home)/dashboard/page.tsx
@@ -1,22 +1,3 @@
-import { Octokit } from "octokit";
-
-const fetchgit = async () => {
-  const octokit = new Octokit({
-    auth: "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",
-  });
-
-  const res = await octokit.request("GET /repos/{owner}/{repo}", {
-    owner: "Laaurent",
-    repo: "OSF-Recouvrement",
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  });
-
-  return res;
-};
-
 export default async function Dashboard() {
-  const result = await fetchgit();
-  return <p>{JSON.stringify(result)}</p>;
+  return <p>Dashboard!</p>;
 }
